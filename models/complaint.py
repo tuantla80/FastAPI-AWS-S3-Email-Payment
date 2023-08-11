deaa6481e3d6f459152a5196caac6f48cba6f77d
Tuan <tuantla80@gmail.com>
2023-08-11 22:11:47 +1000

    add complaint model

---
+++ models/complaint.py
@@ -0,0 +1,18 @@
+import sqlalchemy
+from sqlalchemy import Column, Integer, Float, String, Text, DateTime, Enum, ForeignKey
+
+from db import metadata
+from enums import State
+
+complaint = sqlalchemy.Table(
+    "complaints",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String(120), nullable=False),
+    Column("description", Text, nullable=False),
+    Column("photo_url", String(200), nullable=False),
+    Column("amount", Float, nullable=False),
+    Column("created_at", DateTime, server_default=sqlalchemy.func.utcnow()),
+    Column("status", Enum(State), nullable=False, server_default=State.pending.name),
+    Column("complainer_id", ForeignKey("users.id"), nullable=False)
+)
