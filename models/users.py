c647649f42c6d8e50e643b360244c1b0f1931931
Tuan <tuantla80@gmail.com>
2023-08-11 21:43:52 +1000

    add users model

---
+++ models/users.py
@@ -0,0 +1,20 @@
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, Enum
+
+from db import metadata
+from models.enums import RoleType
+
+user = sqlalchemy.Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("email", String(120), unique=True),
+    Column("password", String(255)),
+    Column("first_name", String(200)),
+    Column("last_name", String(200)),
+    Column("phone_name", String(20)),
+    Column(
+        "role", Enum(RoleType), nullable=False, server_default=RoleType.complainer.name
+    ),
+    Column("iban", String(200)),
+)
