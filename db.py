aadb849e04ba8aa9279fda50b047a05d36a356a4
Tuan <tuantla80@gmail.com>
2023-08-11 21:38:15 +1000

    Add db config

---
+++ db.py
@@ -0,0 +1,14 @@
+import databases
+import sqlalchemy
+from decouple import config
+
+
+DATABASE_URL = f"postgresql://" \
+               f"{config('POSTGRES_USER')}:" \
+               f"{config('POSTGRES_PASSWORD')}" \
+               f"@{config('POSTGRES_HOST')}:" \
+               f"{config('POSTGRES_PORT')}/" \
+               f"{config('POSTGRES_DATABASE')}"
+
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
